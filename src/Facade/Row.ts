c9ee0670c4cc536a41a0a780c9acff764bcccbfc
sangmin <xectler@gmail.com>
2023-09-17 00:32:48 +0900

    facade 패턴

---
+++ src/Facade/Row.ts
@@ -0,0 +1,17 @@
+export default class Row {
+    constructor(private _name: string, private _birthday: string, private _email: string) {
+        
+    }
+
+    get name () {
+        return this._name
+    }
+
+    get birthday () {
+        return this._birthday
+    }
+
+    get email () {
+        return this._email
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, args) => {
     return {
         // devtool: isProd? false : 'eval-source-map',
         // entry: './src/index.ts', // 최상위 자바스크립트 파일
-        entry: './src/prototype/index.ts',
+        entry: './src/facade/index.ts',
         resolve: {
             extensions: ['.ts', '.js']
         },
@@ -47,7 +47,7 @@ module.exports = (env, args) => {
         },
         plugins: [
             new HtmlWebpackPlugin({
-                template: 'public/prototype.html'
+                template: 'public/facade.html'
             }),
             new MiniCssExtractPlugin(),
         ]  
